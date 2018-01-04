5b79335b514620ba224276428d5734bef001a598
josephscottstevens <josephscottstevens@gmail.com>
2018-01-04 13:02:29 -0500

    added type annotations

--- Main.elm
+++ Main.elm
@@ -21,57 +21,53 @@ type Msg
     = DragDropMsg (DragDrop.Msg Int Position)
 
 
+model : Model
 model =
     { data = { count = 0, position = Up }
     , dragDrop = DragDrop.init
     }
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        DragDropMsg msg_ ->
+        DragDropMsg dragMsg ->
             let
-                ( model_, result ) =
-                    DragDrop.update msg_ model.dragDrop
+                ( dragDrop, result ) =
+                    DragDrop.update dragMsg model.dragDrop
             in
-            { model
-                | dragDrop = model_
-                , data =
-                    case result of
-                        Nothing ->
-                            model.data
+                { model
+                    | dragDrop = dragDrop
+                    , data =
+                        case result of
+                            Nothing ->
+                                model.data
 
-                        Just ( count, position ) ->
-                            { count = count + 1, position = position }
-            }
-                ! []
+                            Just ( count, position ) ->
+                                { count = count + 1, position = position }
+                }
+                    ! []
 
 
+divStyle : Attribute msg
 divStyle =
     style [ ( "border", "1px solid black" ), ( "padding", "50px" ), ( "text-align", "center" ) ]
 
 
+view : Model -> Html Msg
 view model =
     let
         dropId =
             DragDrop.getDropId model.dragDrop
     in
-    div []
-        [ viewDiv Up model.data dropId
-        , viewDiv Middle model.data dropId
-        , viewDiv Down model.data dropId
-        ]
+        div []
+            [ viewDiv Up model.data dropId
+            , viewDiv Middle model.data dropId
+            , viewDiv Down model.data dropId
+            ]
 
 
-isNothing maybe =
-    case maybe of
-        Just _ ->
-            False
-
-        Nothing ->
-            True
-
-
+viewDiv : Position -> { a | position : Position, count : Int } -> Maybe Position -> Html Msg
 viewDiv position data dropId =
     let
         highlight =
@@ -80,24 +76,25 @@ viewDiv position data dropId =
             else
                 []
     in
-    div
-        (divStyle
-            :: highlight
-            ++ (if data.position /= position then
-                    DragDrop.droppable DragDropMsg position
-                else
-                    []
-               )
-        )
-        (if data.position == position then
-            [ img (src "https://upload.wikimedia.org/wikipedia/commons/f/f3/Elm_logo.svg" :: width 100 :: DragDrop.draggable DragDropMsg data.count) []
-            , text (toString data.count)
-            ]
-         else
-            []
-        )
+        div
+            (divStyle
+                :: highlight
+                ++ (if data.position /= position then
+                        DragDrop.droppable DragDropMsg position
+                    else
+                        []
+                   )
+            )
+            (if data.position == position then
+                [ img (src "https://upload.wikimedia.org/wikipedia/commons/f/f3/Elm_logo.svg" :: width 100 :: DragDrop.draggable DragDropMsg data.count) []
+                , text (toString data.count)
+                ]
+             else
+                []
+            )
 
 
+main : Program Never Model Msg
 main =
     program
         { init = ( model, Cmd.none )
